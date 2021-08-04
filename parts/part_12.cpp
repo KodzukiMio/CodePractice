a15dff6136f7b00e8e449948e593849e69c5f9cb
KodzukiMio <87706365+KodzukiMio@users.noreply.github.com>
2021-08-04 21:49:04 +0800

    Add files via upload

---
+++ parts/part_12.cpp
@@ -0,0 +1,118 @@
+//#include<SZN_FUC.H>
+//void ms();
+//class t {
+//public:
+//	int x = 7;
+//	t operator+(const t& w)const {
+//		t f;
+//		f.x = this->x + w.x;
+//		return f;
+//	};
+//	friend t operator+(int x,t&w) {
+//		t o;
+//		o.x = x + w.x;
+//		return o;
+//	};
+//	friend t operator+(t& w,int x) {
+//		t o;
+//		o.x = x + w.x;
+//		return o;
+//	};
+//};
+//template<class T>KUR::Stack<T> Sta;
+//template<class T> class PO{
+//private:
+//public:
+//	T* p = NULL;
+//	PO(int n) { p = new T[n]; };
+//};
+//void main_14() {
+//	using namespace std;
+//	t f{ 8 };
+//	t g{ 56 };
+//	std::cout << f.x << g.x;
+//	f = g;
+//	cout << f.x;
+//	t y = f + g;
+//	t u = 7 + g;
+//	t o = g + 7;
+//	cout << endl;
+//	cout << y.x << u.x << o.x;
+//	KUR::Stack<int> wd(10);
+//	wd << 7;
+//	cout << *wd.pop();
+//	8 >> wd;
+//	int d;
+//	wd >> d;
+//	cout << d;
+//	wd << 888;
+//	d << wd;
+//	cout << endl << d;
+//	KUR::Stack<string> E;
+//	"发发无法访问发" >> E;
+//	cout << E;
+//	"\nwdoaifha" >> E;
+//	E >> cout;
+//	KUR::Stack<string>* p = new KUR::Stack<string>[10];
+//	E << "wfaf";
+//	cout <<E << "W大大";
+//	string wfwf;
+//	KUR::Stack<string>Q, W,P;
+//	wfwf << (P << "Wd");
+//	cout << wfwf;
+//	P << "WAFFaWFAFWAF";
+//	Q << "哇发发";
+//	W << "阿瓦达发";
+//	*p = P;
+//	*(p + 1) = Q;
+//	*(p + 2) = W;
+//	cout << *p << *(p + 1) << *(p + 2);
+//	cout << *p;
+//	cout << *p;
+//}
+//class DWP {
+//	private:
+//		int x = 0;
+//		int y = 0;
+//	public:
+//		explicit DWP() { std::cout << "Wds"; };//关闭隐式,必须用显式转换
+//};
+//void main_14() {
+//	using namespace std;
+//	using KUR::SZN_MATH;
+//	SZN_MATH m;
+//	int x,y=0;
+//	cout <<"W" << y;
+//	KUR::Stack<int> i;
+//	i.push(5);
+//	ms();
+//	DWP fw();
+//	int d, df;
+//	d = *i.pop();
+//	cout<<i.pop(df);
+//	cout << endl;
+//	i.push(5);
+//	double ffw = double(i);
+//	cout << ffw;
+//	cout << bool(i);
+//	KUR::Stack<double> w;
+//	w.push(7);
+//	cout << int(w);
+//	Sta<int>.push(6548);
+//	Sta<string>.push("Ws");
+//	cout << *Sta<int>.pop();
+//	cout << *Sta<string>.pop();
+//	//cout << *Sta<int>.pop();
+//	int l;
+//	cout<<(l << Sta<int>);
+//	cout << endl<<Sta<int>.check();
+//	Sta<int>.pop();
+//	cout << endl << Sta<int>.check();
+//	Sta<int>.push(8);
+//	cout << endl << Sta<int>.check();
+//}
+//void ms() {
+//	KUR::Stack<int> i;
+//	i.push(86);
+//	std::cout << *i.pop();
+//}
